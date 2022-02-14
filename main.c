a6f9dc505c3657950fd9a3fdd032b0f12f147ff6
omukoyiaquinter <97959493+omukoyiaquinter@users.noreply.github.com>
2022-02-14 16:40:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+A simple calculator
+by Quinter Omukoyia
+on Feb ,2022
+MIT License
+C89 compiler
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{//variable declarations
+    int nl,n2,sum,diff,product
+    //capture input
+    printf("Enter your name:"
+           gets(name))
+           printf(Enter two integers:"
+                  scanf("d%d",&n1,&n2)
+                  // copulations
+                  sum=n1+n2;
+                  diff=n1-n2;
+                  product=n1*n2;
+                  quot=(float)n1/n2
+
+                  //outputs
+printf("Hey %s,here is your results:\n",name);
+printf("6%d + %6d = %6d\n",n1,n2,sum);
+prinf("6%d - %6d = %6d\n",n1,n2,diff);
+printf("6%d * %6d = %6d\n",n1,n2,product);
+printf("6%d / %6d = %6.2f\n",n1,n2,quot);
+
+
+                        )
+                  )
+    printf("simple calculator!\n");
+    return 0;
+}
